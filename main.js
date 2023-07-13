bc4cf44a750ae6001ed1e0c762bc32d501123f05
RodrigoLV9 <rodrigolv.975@gmail.com>
2023-07-13 20:32:46 -0300

    The calculator has been designed with CSS

---
+++ main.js
@@ -0,0 +1,7 @@
+const display=document.getElementById('display');
+const numbers=document.querySelectorAll('button__number');
+
+/* numbers.forEach((el)=>{
+    console.log(el)
+}) */
+console.log(numbers)
