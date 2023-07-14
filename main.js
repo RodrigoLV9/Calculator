ca3cf5af255a41cfae526ba9b9851be88fafc623
RodrigoLV9 <rodrigolv.975@gmail.com>
2023-07-14 13:39:51 -0300

    La lógica de programación se ha creado con js. Además, se han modificado detalles en el html y css.

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
 const display=document.getElementById('display');
-const numbers=document.querySelectorAll('button__number');
+const numbers=document.querySelectorAll('.button-number');
+const symbol=document.querySelectorAll('.button-symbol')
+const reset=document.getElementById('reset');
+const deleteValue=document.getElementById('delete');
+const equal=document.getElementById('equal');
 
-/* numbers.forEach((el)=>{
-    console.log(el)
-}) */
-console.log(numbers)
+numbers.forEach(element=>{
+    element.addEventListener('click',(e)=>{
+        let numbers=e.target.textContent
+        display.textContent=display.textContent+numbers
+    })
+})
+symbol.forEach(element=>{
+    element.addEventListener('click',(e)=>{
+        let symbols=e.target.textContent
+        display.textContent=display.textContent+symbols
+    })
+})
+reset.addEventListener('click',(e)=>{
+    display.textContent=null
+})
+deleteValue.addEventListener('click',(e)=>{
+    let newValue=display.textContent.slice(0,-1)
+    display.textContent=newValue
+})
+
+
+
+equal.addEventListener('click',e=>{
+    const finalResult=handleErrors(display)
+    display.textContent=finalResult
+})
+
+function handleErrors(display){
+    let result=eval(handleMultiplication(display)).toFixed(2)
+    if(isNaN(result)){
+        return 'Error'
+    }else{
+        return result
+    }
+}
+function handleMultiplication(display){
+    return display.textContent.replace(/x/,"*")
+}
